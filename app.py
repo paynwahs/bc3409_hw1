a12ebf7ad42976038e0fe8d72655800137257881
Yap Yu Xiang <shawnyap99@live.com>
2024-08-26 17:06:08 +0800

    First commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World! This is the first Flask app!'
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
