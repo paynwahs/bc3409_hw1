644c667d232c716f4adeec66240c7a08c3dc8a54
Yap Yu Xiang <shawnyap99@live.com>
2024-10-31 19:21:35 +0800

    HW_4

--- app.py
+++ app.py
@@ -4,19 +4,9 @@ import textblob
 
 app = Flask(__name__)
 
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World! This is the first Flask app!'
-
 @app.route("/",methods=["GET","POST"])
 def sentiment_analysis():
-    return(render_template("sentiment_analysis.html"))
-
-@app.route("/sentiment_analysis_result",methods=["GET","POST"])
-def sentiment_analysis_result():
-    q = request.form.get("q")
-    r=textblob.TextBlob(q).sentiment
-    return(render_template("sentiment_analysis_result.html",r=r))
+    return(render_template("transfer_money.html"))
 
 if __name__ == "__main__":
     app.run(debug=True)
